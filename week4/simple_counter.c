f8f155a5d49b87de68f683f9d8d22b31f8aa4ac4
abdul.arfan <abdul.arfan@gmail.com>
2025-09-26 17:48:38 +0700

    simple counter

--- week4/simple_counter.c
+++ week4/simple_counter.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 
 int main(){
-       int counter = 15;          // initialization
-       while ( counter < 10 ) {     // repetition condition
-            printf( "%d\n", counter );
-            ++counter;             // increment
-       }
+    int counter = 15;          // initialization
+    while ( counter < 10 ) {     // repetition condition
+        printf( "%d\n", counter );
+        ++counter;             // increment
+    }
+
+    return 0;
 }
