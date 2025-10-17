2fce758a6f70732c7534c31b9f86d651d0f68d34
abdul.arfan <abdul.arfan@gmail.com>
2025-10-17 19:35:59 +0700

    swap

--- week7/swap.c
+++ week7/swap.c
@@ -1,5 +1,18 @@
 #include<stdio.h>
 
+void printArr(int arr[], int element) {
+    for (int i=0;i<element;i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void swapElement(int arr[], int a, int b) {
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
+
 int main( ) {
     int a = 10;
     int b = 20;
@@ -11,4 +24,14 @@ int main( ) {
     b = temp;
 
     printf("a=%d, b=%d\n", a, b);
+
+    int arr[] = {1, 2, 3};
+    printArr(arr, 3);
+
+    // int hold = arr[0];
+    // arr[0] = arr[1];
+    // arr[1] = hold;
+    swapElement(arr, 0, 1);
+
+    printArr(arr, 3);
 }
